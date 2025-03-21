98c3632325252a8e6e039109bfb34412ae70d3a5
Siyamj30 <siyamj36@gmail.com>
2025-03-21 11:25:28 +0530

    Add files via upload

---
+++ code list.py
@@ -0,0 +1,37 @@
+# manual
+mybag = [
+    "gun",
+    "bullet",
+    "laptop",
+    "pen",
+    "bottlle",
+    "book",
+    "wallet",
+    "charger",
+    "vaseline",
+    "tape",
+]
+
+print(mybag[0])
+print(mybag[1])
+
+
+fruits = []
+fruits.append("apple")
+fruits.append("mango")
+fruits.append("kiwi")
+fruits.append("banana")
+fruits.append("guava")
+
+mybag.append("phone")
+
+print(fruits)
+print(mybag)
+mybag[5] = 6
+print(mybag)
+
+
+print(len(mybag))
+
+mybag[0] = "flower"
+mybag[1] = "candy"
